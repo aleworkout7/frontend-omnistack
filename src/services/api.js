872ccafeecbd4def8db186913272bc83dc31dde1
aleworkout <alegonznav.int@gmail.com>
2019-04-13 13:48:32 -0400

    apagando parametro localhost da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'http://localhost:3333' || 'https://omnistack-chile-frontend.herokuapp.com',
+	baseURL: 'https://omnistack-chile-frontend.herokuapp.com',
 });
 
 export default api;
